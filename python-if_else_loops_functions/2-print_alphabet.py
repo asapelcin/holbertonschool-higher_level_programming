3a530eed8078d0692d488b94a7b717991375713a
asapelcin <13572@holbertonstudents.com>
2026-04-04 16:49:30 +0400

    hexsabayt

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-print("abcdefghijklmnopqrstuvwxyz")
+print("{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}" .format(
+    chr(97), chr(98), chr(99), chr(100), chr(101), chr(102),
+    chr(103), chr(104), chr(105), chr(106), chr(107), chr(108),
+    chr(109), chr(110), chr(111), chr(112), chr(113), chr(114),
+    chr(115), chr(116), chr(117), chr(118), chr(119), chr(120),
+    chr(121), chr(122)))
