1f37d8f0c129e5415aef9a57e8e3cdfbb275b8e6
asapelcin <13572@holbertonstudents.com>
2026-04-04 17:11:47 +0400

    Fix print count in uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,7 @@
 def uppercase(str):
     """Print string in uppercase."""
     for c in str:
-        if ord('a') <= ord(c) <= ord('z'):
-            print("{}".format(chr(ord(c) - 32)), end="")
-        else:
-            print("{}".format(c), end="")
+        print("{}".format(
+            chr(ord(c) - 32) if ord('a') <= ord(c) <= ord('z') else c
+        ), end="")
     print("")
