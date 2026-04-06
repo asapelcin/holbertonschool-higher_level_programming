733c4b75be91bbb63a24bf52df0d021d654453ce
asapelcin <13572@holbertonstudents.com>
2026-04-06 22:40:04 +0400

    Fix: pycodestyle line too long

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,15 +2,20 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    roman_dict = {
+
+    rd = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
     }
+
     total = 0
-    for i in range(len(roman_string)):
-        current_val = roman_dict.get(roman_string[i], 0)
-        if i + 1 < len(roman_string) and roman_dict.get(roman_string[i + 1], 0) > current_val:
-            total -= current_val
+    length = len(roman_string)
+
+    for i in range(length):
+        cur = rd.get(roman_string[i], 0)
+        if i + 1 < length and rd.get(roman_string[i + 1], 0) > cur:
+            total -= cur
         else:
-            total += current_val
+            total += cur
+
     return total
