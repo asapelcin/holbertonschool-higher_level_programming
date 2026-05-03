e877ea0cf6773a3b408aa98f29055f4fc13eb896
asapelcin <13572@holbertonstudents.com>
2026-05-03 17:36:09 +0400

    Fix pycodestyle in inherits_from

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,13 +3,13 @@
 
 
 def inherits_from(obj, a_class):
-    """Returns True if obj is an instance of a class that inherited from a_class.
+    """Returns True if obj is instance of a subclass of a_class.
 
     Args:
         obj: The object to check.
         a_class: The class to check against.
 
     Returns:
-        bool: True if obj is instance of a subclass of a_class, else False.
+        bool: True if inherited, False otherwise.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
