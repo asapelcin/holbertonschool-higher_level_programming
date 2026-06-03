8366c0eba742e52abc5d0eb527e0cf0a1fa26473
asapelcin <13572@holbertonstudents.com>
2026-06-03 13:47:57 +0400

    Add from json string function

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Convert JSON string to object."""
+import json
+
+
+def from_json_string(my_str):
+    """Return Python object represented by a JSON string."""
+    return json.loads(my_str)
