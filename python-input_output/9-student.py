66de7cfc357cd95cae76733758dfa80552d3a543
asapelcin <13572@holbertonstudents.com>
2026-06-03 13:57:01 +0400

    Add Student class

---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Student class module."""
+
+
+class Student:
+    """Student class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return dictionary representation of Student."""
+        return self.__dict__
